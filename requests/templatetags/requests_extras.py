7c3ab7f3f70018c07d1befa62e255f7b335614f3
temaia <teresa.maia@vib-ugent.be>
2019-02-17 21:53:43 +0100

    urlsANDsettingsupdate

--- requests/templatetags/requests_extras.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def addstr(str1):
-	"""concantenate str1 & str2"""
-	return '../media/ED/Experimental_design_' + str(str1) + '.xlsx'
--- requests/untitled.py
+++
@@ -1,6 +0,0 @@
-users=User.objects.all()
-user1 = users[0]
-user1.profile.Analysis_Type
-
-
-temp = Profile.objects.get(issue=self.request.user.profile.Issue)
